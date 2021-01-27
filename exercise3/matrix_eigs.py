dea1e2e5fdfbc781174819f5e3eabc727c6d60f1
Matias Hiillos <matias.hiillos@gmail.com>
2021-01-27 16:05:50 +0200

    Problem 3

--- exercise3/matrix_eigs.py
+++ exercise3/matrix_eigs.py
@@ -32,6 +32,29 @@ def largest_eig(A,tol=1e-12):
     - Also good commenting.
     - You may modify the output / return values if you like
     """
+
+    # Initialize eigenvector, eigenvalue and the precision
+    eig_vector = np.ones(A.shape[0])
+    eig_value = 0.
+    prec = 2*tol
+
+    # Using the power method until at precision
+    while prec > tol:
+      vk = A.dot(eig_vector) # A*x
+
+      # Normalizing the vector 
+      x_norm = np.linalg.norm(vk)
+      x_1 = vk/x_norm
+
+      # Updating the eigenvalue, A*A*x etc.
+      eig_value = x_1.dot(A.dot(x_1))
+
+      # Updating the precision for tolerance check
+      prec = np.linalg.norm(vk-eig_value*eig_vector)
+
+      # Updating eigenvector
+      eig_vector = x_1
+
     return eig_value, eig_vector
 
 
@@ -49,7 +72,7 @@ def main():
             -0.5 / dx2 * np.ones(grid_size - 1)
         ],
         [-1, 0, 1])
-    
+
     # use scipy to calculate the largest eigenvalue
     # and corresponding vector
     eigs, evecs = sla.eigsh(H0, k=1, which='LA')
@@ -67,7 +90,9 @@ def main():
     psi0=psi0/norm_const
     
     # largest_eig eigen vector 
-    psi0_=vec[:,0]
+    # At the moment works without [:,0] as the current power method function
+    # only returns the eigenvector corresponding to the largest eigenvalue 
+    psi0_=vec
     norm_const=simps(abs(psi0_)**2,x=grid)
     psi0_=psi0_/norm_const
     
